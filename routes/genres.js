5fe7d131e01cf9dbc7b3c62b7ea27a67175285bd
Nonso Wogu <recreatesuccess@gmail.com>
2019-11-22 18:56:40 -0500

    Connection to MongoDB enabled

---
+++ routes/genres.js
@@ -0,0 +1,56 @@
+const { Genre, validate } = require('../models/genres')
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+
+/* Old hard-coded initialization:
+  const genres = [
+    { id: 1, name: 'Action' },  
+    { id: 2, name: 'Horror' },  
+    { id: 3, name: 'Romance' },  
+  ]; */
+
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.send(genres);
+  });
+  
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
+    res.send(genre);
+  });
+  
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
+  });
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
+});
+  
+router.delete('/:id', async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
+});
+  
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+  
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
+  res.send(genre);
+});
+
+module.exports = router;
